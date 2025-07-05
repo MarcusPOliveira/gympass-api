8309eefb4c5edc489c60f1856fd5d09323b06f2b
MarcusPOliveira <mp.mp.oliveira@gmail.com>
2025-07-05 15:18:11 -0300

    feat: add PostgreSQL setup and prisma schema

--- src/server.ts
+++ src/server.ts
@@ -2,5 +2,5 @@ import { app } from './app'
 import { env } from './env'
 
 app.listen({ port: env.PORT, host: '0.0.0.0' }).then(() => {
-  console.log('HTTP server running! 🚀')
+  console.log('HTTP server running! 🚀', process.env.DATABASE_URL)
 })
